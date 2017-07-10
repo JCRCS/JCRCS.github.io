1acf2e5bb9c348c811bb488bb1eaa1506c1ae86d
rossolutions <juancarlosrosito@gmail.com>
2017-07-10 17:23:30 -0600

    Update saveContactInfo.php

--- saveContactInfo.php
+++ saveContactInfo.php
@@ -5,5 +5,5 @@ $name=$_POST["userName"];
 $email=$_POST["userMail"];
 $mobile$_POST["userMobile"];
 $address=$_POST["userAddress"];
-mysqli_query($connect,"INSERT INTO contact_info(userName,userEmail,userMobile,userAddress) VALUES('$name','$email','$mobile','$address')")
+mysqli_query($connect,"INSERT INTO contact_info(userName,userEmail,userMobile,userAddress) VALUES('$name','$email','$mobile','$address')");
 >?
